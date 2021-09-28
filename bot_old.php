c1846e9012ad1c1f1b44439d9500ffd368a5ec1f
Alexander Soromotin <a-sorom@mail.ru>
2021-09-28 17:55:10 +0500

    28 sep 2021

--- bot_old.php
+++ bot_old.php
@@ -1,12 +1,11 @@
 <?php
-error_reporting(E_ALL);
-ob_start();
-// Hi ICUE | Bot
-include "db.php";
+// Mega IQ | Bot
 include "settings.php";
+include "db.php";
 
 // Отправка сообщения (текст сообщение)
 function sendMessage ($text) {
+
 	global $access_token;
 	global $chat_id;
 	global $apiVer;
@@ -29,6 +28,7 @@ function cutString ($string, $cutPie) {
 
 // Получение информации о пользователе из ВК
 function getUserInfoFromVK ($type) {
+
 	global $user_id;
 	global $access_token;
 	global $apiVer;
@@ -62,6 +62,7 @@ function getUserInfoFromVK ($type) {
 
 // Получение информации о пользователе из БД
 function getUserInfoFromDB ($type) {
+
 	global $connection;
 	global $user_id;
 
@@ -74,10 +75,10 @@ function getUserInfoFromDB ($type) {
 		return $user['user_id'];
 	}
 	if ($type == 'first_name') {
-		return $user['first_name'];
+		return addslashes($user['first_name']);
 	}
 	if ($type == 'last_name') {
-		return $user['last_name'];
+		return addslashes($user['last_name']);
 	}
 	if ($type == 'iq') {
 		return $user['iq'];
@@ -89,9 +90,36 @@ function getUserInfoFromDB ($type) {
 		return $user['cmd_date'];
 	}
 	if ($type == 'both_name') {
-		return $user['first_name'] . ' ' . $user['last_name'];
+		return addslashes($user['first_name']) . ' ' . addslashes($user['last_name']);
 	}
 }
+
+// function getChatInfoFromVK () {
+// 	// Не работает!!!!!!!!!
+// 	global $apiVer;
+// 	global $user_token;
+// 	global $group_token;
+// 	global $group_id;
+// 	global $access_token;
+
+// 	$request_params = array(
+// 		'chat_id' => '1',
+// 		'v' => $apiVer,
+// 		'access_token' => $user_token,
+// 		'fields' => 'nickname'
+// 	);
+
+// 	$get_params = http_build_query($request_params);
+// 	// $result = json_decode(file_get_contents('https://api.vk.com/method/messages.getChat?'. $get_params));
+// 	$result = file_get_contents('https://api.vk.com/method/messages.getChat?'. $get_params);
+
+// 	// if ($type == 'title') {
+// 	// 	return $result -> response[0] -> title;;
+// 	// }
+// 	// echo $access_token;
+// 	print_r($result);
+// 	return $result;
+// }
 
 // Проверка регестрации пользователя
 function checkUserRegestration ($action) {
@@ -239,16 +267,7 @@ function updateTotalIqInChat () {
 
 	return $count;
 }
-function userIsAdmin () {
-	global $user_id;
-	global $admins;
 
-	if (gettype(array_search($user_id, $admins)) == "boolean") {
-		return false;
-	} else {
-		return true;
-	}
-}
 
 
 
@@ -264,94 +283,31 @@ switch ($data->type) {
 	
 	// Событие: Новое сообщение
 	case 'message_new': 
-		echo "ok";
+
 		$message_text = mb_strtolower($data -> object -> text); 	// Сообщение пользователя
 		$chat_id = $data -> object ->  peer_id; 					// Идентификатор назначения
 		$user_id = $data -> object -> from_id; 
 
-		if (isset($data -> object -> action)) {
-			$action_type = $data -> object -> action -> type;
-			$action_member_id = $data -> object -> action -> member_id;
 
-			if ($action_type == "chat_invite_user" and $action_member_id == (-1) * $group_id) {
-				sendMessage("Всем привет! Из-за ограничений ВК я не смогу писать, если вы не дадите мне права администратора!
-	Чтобы узнать все возмоности используйте команды -инфо или !инфо");
-				break;
-			}
-		}
-
-		// Список функций
-		$functions = array(
-			"increase_iq" => array("!iq", "-iq"),
-			"display_top" => array("!топ", "-топ", "!top", "-top"),
-			"display_general_iq" => array("!чат", "-чат", "!chat", "-chat"),
-			"display_user_iq" => array("!мой", "-мой", "!my", "-my", "!my iq", "-my iq"),
-			"display_info" => array("!инфо", "-инфо", "!info", "-info", "!помощь", "-помощь", "!help", "-help", "Начать"),
-			
-			// For admins
-			"active_break" => array("!тех вкл", "-тех вкл", "!включить перерыв", "-включить перерыв"),
-			"deactive_break" => array("!тех выкл", "-тех выкл", "!выключить перерыв", "-выключить перерыв"),
-			"display_bot_status" => array("!тех статус", "-тех статус"),
-			"display_info_for_admins" => array("!тех", "-тех"),
-			"display_full_top" => array("!тех топ", "-тех топ")
-		);
-
-		function findTextInArray ($text) {
-		    global $functions;
-		    
-		    foreach ($functions as $function_name => $tags) {
-		        // echo $function_name . "\n";
-			    foreach($functions[$function_name] as $key2 => $value2) {
-			       // echo "... " . $value2 . "\n";
-			        if ($value2 == $text) {
-			           // echo "... " . $value2 . "\n";
-			           //echo $function_name;
-		        		return $function_name;
-			        }
-			    }
-		    }
-		}
-		// Обращение к боту подтверждено
-		if (findTextInArray($message_text) != "") {
-			checkUserInTheChat();
-		}
-
-
-
-		if (!userIsAdmin()) {
-			// break;
-		}
-
-
-		// Если перерыв и пользователь не является админом, то break;
-		if ($break == 1 and !userIsAdmin()) {
-			if (findTextInArray($message_text) != "") {
-					$function_name = findTextInArray($message_text);
-
-				mysqli_query($connection, "INSERT INTO `acts_during_break` (`user_id`, `function`, `chat_id`) VALUES ('$user_id', '$function_name', '$chat_id') ");
-
-				sendMessage('На данный момент на сервере ведутся тех. работы. Обычно, это занимает пару часов. Ваше обращение записано, после окончания тех. работ оно будет обработано, приносим извинения за представленные неудобства.');
-			}
-			// echo "ok";
-			// exit();
-			// die();
+		if (strpos($user_id, '-') !== false) {
 			break;
-			// exit();
+			echo "ok";
 		}
+
+		global $admins;
+
+		// sendMessage(array_search($user_id, $admins));
+		if ($break == true and gettype(array_search($user_id, $admins)) == "boolean") {
+
+			break;
+			echo "ok";
+		}
+	
 		
 
-		// Если боту пишет какое-то сообщество, то происходит игнор
-		if (strpos($user_id, '-') !== false) {
-			// echo "ok";
-			// exit();
-			break;
-		}
-
-		function increase_iq () {
-			global $user_id;
-			global $chat_id;
-			global $message_text;
-			global $connection;
+		// Манипуляции с iq
+		if ($message_text == "!iq" or $message_text == "-iq") {
+			checkUserInTheChat();
 
 			// Время прошлого использования команды /iq
 			$cmd_date = explode(', ', getUserInfoFromDB('cmd_date'));
@@ -383,13 +339,13 @@ switch ($data->type) {
 			// sendMessage($current_d . ' - ' . $cmd_d);
 			if (getUserInfoFromDB('cmd_date') == '' or $flag == 1) {
 				$iq = getUserInfoFromDB('iq');
-				$type = rand(1, 3);
+				$type = rand(1, 4);
 
-				if ($iq <= 10) {
+				if ($iq <= 0) {
 					$type = 1;
 				}
 
-				if ($type == 3) {
+				if ($type == 4) {
 					// Не повезло, iq уменьшился;
 					$rand = rand(1, 9);
 					if ($iq < $rand) {
@@ -413,13 +369,9 @@ switch ($data->type) {
 			}
 		}
 
-		function display_top () {
-			global $user_id;
-			global $chat_id;
-			global $message_text;
-			global $connection;
-
-			// sendMessage($chat_id);
+		// Отображение рейтинга
+		if ($message_text == "!топ" or $message_text == "!top" or $message_text == "-топ" or $message_text == "-top") {
+			checkUserInTheChat();
 
 			// Получение массива пользователей беседы
 			// Формат ( ID1, ID2, ... IDn )
@@ -459,63 +411,48 @@ switch ($data->type) {
 			 	$count++;
 			};
 			sendMessage($text);
+
+			// echo $text;
 		}
 
-		function display_full_top () {
-			global $user_id;
-			global $chat_id;
-			global $message_text;
-			global $connection;
+		// Общий топ
+// 		else if ($message_text == "!топ все" or $message_text == "!top all") {
+// 			checkUserInTheChat();
 
-			$users = mysqli_query($connection, "SELECT * FROM `users` ORDER BY `iq` DESC LIMIT 0, 50");
+// 			$users = mysqli_query($connection, "SELECT * FROM `users` ORDER BY `iq` DESC LIMIT 0, 10");
 
-			$text = 'Общий рейтинг (1 - 50):
-';
-			$count = 1;
-			while ($user = mysqli_fetch_assoc($users)) {
-				$first_name = $user['first_name'];
-				$last_name = $user['last_name'];
-				$iq = $user['iq'];
-				$user_id_local = $user['user_id'];
+// 			$text = 'Общий рейтинг:
+// ';
+// 			$count = 1;
+// 			while ($user = mysqli_fetch_assoc($users)) {
+// 				$first_name = $user['first_name'];
+// 				$last_name = $user['last_name'];
+// 				$iq = $user['iq'];
 
-				if ($user['joined'] == true) {
-					$join_status = "&#9989;";
-				} else {
-					$join_status = "&#9940;";
-				}
+// 				$text = $text . $count . '. ' . $first_name . ' ' . $last_name . ' - ' . $iq . ' iq 
+// ';
+// 				$count++;
+// 			}
 
-				$text = $text . $join_status . $count . '. [id' . $user_id_local . '|' . $first_name . ' ' . $last_name . '] - ' . $iq . ' iq 
-';
-				$count++;
-			}
+// 			sendMessage($text);
+// 		}
 
-			sendMessage($text);
-		}
-
-		function display_general_iq () {
-			global $user_id;
-			global $chat_id;
-			global $message_text;
-			global $connection;
+		// Общая составляющая интеллекта беседы
+		else if ($message_text == "!чат" or $message_text == "!chat" or $message_text == "-чат" or $message_text == "-chat") {
+			checkUserInTheChat();
 
 			sendMessage('Общий интеллект вашей группы: ' . updateTotalIqInChat() . ' iq');
 		}
 
-		function display_user_iq () {
-			global $user_id;
-			global $chat_id;
-			global $message_text;
-			global $connection;
+		// Отображение iq пользователя
+		else if ($message_text == "!my iq" or $message_text == "!мой iq" or $message_text == "!мой интеллект" or $message_text == "!мой" or $message_text == "!отобрази мой интеллект" or $message_text == '!покажи мой интеллект' or $message_text == "-my iq" or $message_text == "-мой iq" or $message_text == "-мой интеллект" or $message_text == "-мой" or $message_text == "-my") {
+			checkUserInTheChat();
 
 			sendMessage(getUserInfoFromDB('both_name') . ', ваш интеллект составляет ' . getUserInfoFromDB('iq') . ' iq');
 		}
 
-		function display_info () {
-			global $user_id;
-			global $chat_id;
-			global $message_text;
-			global $connection;
-
+		// Информация о возможностях бота
+		else if ($message_text == "!info" or $message_text == "!инфо" or $message_text == "!information" or $message_text == "!информация" or $message_text == "!help" or $message_text == "!помощь" or $message_text == "-info" or $message_text == "-инфо" or $message_text == "-help" or $message_text == "-помощь") {
 			sendMessage('
 
 &#128073;&#127995; -iq  (Повышение IQ (!iq))
@@ -524,233 +461,6 @@ switch ($data->type) {
 &#128073;&#127995; -инфо  (Список возможностей (!инфо))
 ');
 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
-		
-
-		// Включение перерыва (Тех. работ)
-		if (findTextInArray($message_text) == "active_break" and userIsAdmin()) {
-			mysqli_query($connection, "UPDATE `settings` SET `break` = 1 ");
-			sendMessage('Технические работы активированы, бот отвечает только админам. Записываются обращения пользователей.');
-		}
-		// Отключение перерыва (Тех. работ)
-		if (findTextInArray($message_text) == "deactive_break" and userIsAdmin()) {
-			mysqli_query($connection, "UPDATE `settings` SET `break` = 0 ");
-
-			$array = mysqli_query($connection, "SELECT * FROM `acts_during_break` WHERE `status` = 1");
-			
-			$users_array = array();
-			$users_array_with_functions = array();
-			// для избежания повторяющихся запросов пройдём через два фильтра
-
-			// Создание массива пользователей, обратившихся к боту ($users_array)
-			$count = 0;
-			while ($r = mysqli_fetch_assoc($array)) {
-				$count++;
-				if (!in_array($r['user_id'], $users_array)) {
-					array_push($users_array, $r['user_id']);
-				}
-			}
-
-			// Создание второго массива, ассоциация пользователя с функциями, которые первый запросил у бота (&users_array_with_functions)
-			foreach ($users_array as $value) {
-				$local_functions = mysqli_query($connection, "SELECT * FROM `acts_during_break` WHERE `user_id` = '$value'");
-				$users_array_with_functions[$value]['functions'] = array();
-				
-				// sendMessage( $value . " | chatId: " . $users_array_with_functions[$value]['chat_id']);
-
-				while ($l = mysqli_fetch_assoc($local_functions)) {
-					$users_array_with_functions[$value]['chat_id'] = $l['chat_id'];
-					if ( !in_array( $l['function'], $users_array_with_functions[$l['user_id']]['functions'] ) ) {
-						// sendMessage($l['function'] . " pushed in " . $l['user_id']);
-						array_push( $users_array_with_functions[$l['user_id']]['functions'], $l['function'] );
-					}
-				}
-			}
-
-			// sendMessage($users_array_with_functions);
-			// var_dump($users_array_with_functions);
-
-			sendMessage('Технические работы остановлены, бот отвечает всем. В обработке ' . $count . ' обращений.');
-
-			// Исполнение всех неповторяющихся запросов пользователей
-			foreach ($users_array_with_functions as $key => $value) {
-
-				$GLOBALS['user_id'] = $key;
-				$GLOBALS['chat_id'] = $value['chat_id'];
-				
-
-				foreach ($value['functions'] as $functionName) {
-
-					if ($functionName == 'increase_iq') {
-						increase_iq();
-					}
-					if ($functionName == 'display_top') {
-						display_top();
-					}
-					if ($functionName == 'display_general_iq') {
-						display_general_iq();
-					}
-					if ($functionName == 'display_user_iq') {
-						display_user_iq();
-					}
-					if ($functionName == 'display_info') {
-						display_info();
-					}
-				}
-			}
-			mysqli_query($connection, "DELETE FROM `acts_during_break`");
-
-
-
-
-
-
-
-
-			// $act = mysqli_query($connection, "SELECT * FROM `acts_during_break`");
-			
-
-			
-
-			// while ($g = mysqli_fetch_assoc($act)) {
-
-				
-
-				// $id = $g['id'];
-				// $GLOBALS['user_id'] = $g['user_id'];
-				// $GLOBALS['chat_id'] = $g['chat_id'];
-				// $function = $g['function'];
-
-				// if ($function == 'increase_iq') {
-					// increase_iq();
-				// }
-				// if ($function == 'display_top') {
-					// display_top();
-				// }
-				// if ($function == 'display_general_iq') {
-					// display_general_iq();
-				// }
-				// if ($function == 'display_user_iq') {
-					// display_user_iq();
-				// }
-				// if ($function == 'display_info') {
-					// display_info();
-				// }
-
-				// mysqli_query($connection, "DELETE FROM `acts_during_break` WHERE `id` = '$id'");
-				
-			// }
-
-			// sendMessage('Технические работы остановлены, бот отвечает всем. Выполнено ' . $count . ' обращений, оставленных во время перерыва.');
-		}
-		// Общие параметры бота и группы
-		if (findTextInArray($message_text) == "display_bot_status" and userIsAdmin()) {
-			$text = "";
-			$usersCount = mysqli_fetch_array( mysqli_query($connection, "SELECT COUNT(*) FROM `users` ") )[0];
-			$usersAreJoinedInGroup = mysqli_fetch_array( mysqli_query($connection, "SELECT COUNT(*) FROM `users` WHERE `joined` = 'true' ") )[0];
-			
-			if ($break == 1) {
-				$text = $text . "Технические работы активированы.
-";
-			} else {
-				$text = $text . "Технические работы остановлены.
-";
-			}
-			$text = $text . "Пользователи: " . $usersCount . " чел.
-Подписаны на группу: " . $usersAreJoinedInGroup . "/" . $usersCount . " чел.";
-
-			sendMessage($text);
-		}
-		if (findTextInArray($message_text) == "display_info_for_admins" and userIsAdmin()) {
-			sendMessage('-тех вкл (вкл. перерыв)
--тех выкл (выкл. перерыв)
--тех статус (общие параметры)
--тех (справка по командам)');
-		}
-
-		
-		// Манипуляции с iq
-		if (findTextInArray($message_text) == "increase_iq") {
-			increase_iq();
-		}
-		// Отображение рейтинга
-		if (findTextInArray($message_text) == "display_top") {
-			display_top();
-		}
-		// Общий топ
-		if (findTextInArray($message_text) == "display_full_top") {
-			display_full_top();
-		}
-
-		// Общая составляющая интеллекта беседы
-		else if (findTextInArray($message_text) == "display_general_iq") {
-			display_general_iq();
-		}
-
-		// Отображение iq пользователя
-		else if (findTextInArray($message_text) == "display_user_iq") {
-			display_user_iq();
-		}
-
-		// Информация о возможностях бота
-		else if (findTextInArray($message_text) == "display_info") {
-			display_info();
-		}
-
-		if (strpos($message_text, 'jkl') !== false) {
-			$array = mysqli_query($connection, "SELECT * FROM `acts_during_break`");
-
-			$users_array = array();
-			$users_array_with_functions = array();
-			while ($r = mysqli_fetch_assoc($array)) {
-				// sendMessage($r['user_id']);
-				if (!in_array($r['user_id'], $users_array)) {
-					array_push($users_array, $r['user_id']);
-				}
-			}
-
-			// sendMessage("1 array: " . json_encode($users_array));
-			// var_dump($users_array);
-
-			foreach ($users_array as $value) {
-				$functions = mysqli_query($connection, "SELECT * FROM `acts_during_break` WHERE `user_id` = '$value'");
-				$users_array_with_functions[$value] = array();
-				// sendMessage("GG - " . $value);
-				while ($l = mysqli_fetch_assoc($functions)) {
-
-					if ( !in_array( $l['function'], $users_array_with_functions[$l['user_id']] ) ) {
-						// sendMessage($l['function'] . " pushed in " . $l['user_id']);
-						array_push( $users_array_with_functions[$l['user_id']], $l['function'] );
-					}
-				}
-			}
-		}
-
 
 
 
@@ -766,7 +476,7 @@ switch ($data->type) {
 		// 	sendMessage('');
 		// }
 
-	// echo 'ok';
+	echo 'ok';
 	break;
 
 	// Новая запись на стене сообщества
@@ -815,7 +525,7 @@ switch ($data->type) {
 			mysqli_query($connection, "UPDATE `users` SET `iq` = `iq` + 20, `joined` = 'true' WHERE `user_id` = '$user_id' ");
 
 			sendMessage('Добро пожаловать в клуб анонимных интеллектуалов, вам начислено 20 iq! 
-Для получения информации о командах напишите -инфо ');
+Для получения информации о командах напишите !инфо ');
 		}
 
 		
